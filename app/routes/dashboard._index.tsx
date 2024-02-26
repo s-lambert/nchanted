c6ddcc10266f0d1f84e9b5759b0133ff6f18cc87
Scott Lambert <s-lambert@live.com.au>
2024-02-26 19:21:42 +0800

    Modify chart to be have projection/target

--- app/routes/dashboard._index.tsx
+++ app/routes/dashboard._index.tsx
@@ -1,6 +1,6 @@
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { Area, AreaChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, ReferenceLine, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import { getCodeCoverage } from '~/models/code-coverage.server';
 import { card, cardHeader } from './dashboard_index.css';
 
@@ -10,8 +10,12 @@ export const loader = async () => {
   });
 };
 
+const goalPercent = 40;
+const projectEnd = new Date('2024/03/22').getTime();
+
 export default function Dashboard() {
   const { codeCoverage } = useLoaderData<typeof loader>();
+  const start = codeCoverage[0];
 
   return (
     <main>
@@ -20,21 +24,33 @@ export default function Dashboard() {
         <h2 className={cardHeader}>Code Coverage</h2>
         <div style={{ height: '100%', width: '100%' }}>
           <ResponsiveContainer>
-            <AreaChart margin={{ top: 5, left: 0, right: 5, bottom: 0 }} data={codeCoverage}>
+            <AreaChart margin={{ top: 6, left: 0, right: 5, bottom: 0 }} data={codeCoverage}>
               <defs>
                 <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
                   <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
                 </linearGradient>
               </defs>
-              <YAxis width={28} stroke="white" />
+              <YAxis width={28} stroke="white" type="number" domain={[30, goalPercent]} />
               <XAxis
+                type="number"
+                domain={[start.time, projectEnd]}
                 dataKey="time"
                 tickFormatter={(x) => new Date(x).toLocaleDateString('en-AU', { month: 'numeric', day: 'numeric' })}
                 height={50}
                 angle={-45}
                 textAnchor="end"
                 stroke="white"
+              />
+              <ReferenceLine y={goalPercent} label="Target" stroke="red" strokeDasharray="3 3" />
+              <ReferenceLine
+                label="Projected"
+                stroke="green"
+                strokeDasharray="3 3"
+                segment={[
+                  { x: start.time, y: start.statementPercent },
+                  { x: projectEnd, y: goalPercent },
+                ]}
               />
               <Area dot dataKey="statementPercent" stroke="white" fillOpacity={1} fill="url(#colorPv)" />
             </AreaChart>
